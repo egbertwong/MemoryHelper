bec7c9d15520d4bf7d6787cb774781797a22aafe
egbertwong <651020224@qq.com>
2020-08-23 01:23:54 +0800

    feat: add today page function framework

---
+++ TodayController.js
@@ -0,0 +1,30 @@
+/**
+ * Call this function at the beginning of the page to load the today interface.
+ */
+function loadTodayInterface() {
+    html = ``
+}
+
+/**
+ * Call this function when switched in Today page.
+ * @param {scheduled list} todayList Filter out today's tasks from the scheduled list
+ */
+function loadTodayList(todayList) {
+
+}
+
+/**
+ * Open detail column and show the details of the clicking item.
+ * @param {scheduled item} item 
+ */
+function loadTodayItemDetails(item) {
+    $('#today-details').css("display","")
+}
+
+/**
+ * Remove the finishing task from page and move task in the database.
+ * @param {int} id Id of the task
+ */
+function finishTodayItem(id) {
+
+}
